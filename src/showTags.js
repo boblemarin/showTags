464dbd32baa68b02c1d02b3b8b1d169f8cd0148d
Sébastien Geelen <seb@woorank.com>
2014-02-16 22:54:49 +0100

    the minified file is now an actual valid bookmaklet

--- src/showTags.js
+++ src/showTags.js
@@ -1,28 +1,42 @@
+function st () {
 
-// One Script to rule them all, One Script to find them,
-// One Script to bring them all and in the HTML bind them
-
-(function () {
+  // One Script to rule them all, One Script to find them,
+  // One Script to bring them all and in the HTML bind them
 
   // rule Them
   var doc             = document,
       style           = doc.createElement('style'),
       tagsToHighlight = ('h1 h2 h3 h4 h5 h6 h7 a p div span img blockquote form input strong em i b u strong em code pre section article footer header nav aside').split(' '),
-      commonStyle     = 'font-size:10px;font-family:monospace;padding:1px 2px;vertical-align:middle;opacity:.4;border-radius:5px;color:#A00;background:rgba(255,255,255,.7);position:absolute;}',
+      commonStyle     = '{font-size:10px;font-family:monospace;padding:1px 2px;vertical-align:middle;opacity:.4;border-radius:5px;color:#A00;background:rgba(255,255,255,.7);position:absolute;}',
+      hoverStyle      = '{position:relative;opacity:.8}',
       styleInner      = '',
+      styleInnerB     = '',
+      styleInnerA     = '',
+      styleInnerH     = '',
+      glue            = '',
       i, tagTmp;
 
   // find Them
   for (i in tagsToHighlight) {
     tagTmp      = tagsToHighlight[i];
 
-    styleInner += tagTmp + ':before{content:\42\\003c ' + tagTmp + '\\003e\42;' + commonStyle;
-    styleInner += tagTmp + ':after{content:\42\\003c/' +tagTmp + '\\003e\42;' + commonStyle;
-    styleInner += tagTmp + ':hover:after, :hover:before{position:relative;opacity:.8}';
+    styleInner += tagTmp + ':before{content:\42\\003c ' + tagTmp + '\\003e\42;}';
+    styleInner += tagTmp + ':after{content:\42\\003c/' +tagTmp + '\\003e\42;}';
+
+    styleInnerB += glue + tagTmp + ':before';
+    styleInnerA += glue + tagTmp + ':after';
+    styleInnerH += glue + tagTmp + ':hover:after, ' + tagTmp + ':hover:before';
+
+    glue = ', ';
   }
+
+  styleInner += styleInnerB + commonStyle;
+  styleInner += styleInnerA + commonStyle;
+  styleInner += styleInnerH + hoverStyle;
 
   // bind Them
   style.innerHTML = styleInner;
   doc.head.appendChild(style);
 
-})();
+};
+st();
